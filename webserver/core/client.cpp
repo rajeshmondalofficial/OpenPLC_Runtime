80fa71c9fac1739184091ba3f985bdd74735c9b3
RAJESH MONDAL <rajeshmondal9007@gmail.com>
2024-12-06 00:16:33 +0530

    Update client.cpp

--- webserver/core/client.cpp
+++ webserver/core/client.cpp
@@ -130,6 +130,8 @@ void *uart_listener_thread(void *arg) {
     char buffer[256];
     while (1) {
         int bytes_read = read(global_uart_fd, buffer, sizeof(buffer) - 1);
+        sprintf(log_msg, "UART: Connection Receive: => %d\n", bytes_read);
+        log(log_msg);
         if (bytes_read > 0) {
             buffer[bytes_read] = '\0'; // Null-terminate the received string
             // Lock the mutex to update shared data
