4c79bd2776544e437997530445a402c9039e51ad
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-15 20:25:48 +0530

    Rajesh: Logs

--- webserver/core/client.cpp
+++ webserver/core/client.cpp
@@ -364,6 +364,7 @@ int rylr998_config(uint8_t *device, int baud_rate, int frequency) {
     char at_command[] = "AT+BAND=";
     char message[256];
     char numStr[32]; 
+    char log_msg[1000];
     // Convert the integer to a string
     sprintf(numStr, "%d", frequency);
     strcat(at_command, numStr);
@@ -376,8 +377,8 @@ int rylr998_config(uint8_t *device, int baud_rate, int frequency) {
     
     if(byte_read > 0) {
         message[byte_read] = '\0';
-        // sprintf(log_msg, "RYLR998: Configuration Success: => %s\n", message);
-        log("RYLR998: Configuration Success: => %s\n", message);
+        sprintf(log_msg, "RYLR998: Configuration Success: => %s\n", message);
+        log(log_msg);
         return 1;
     }
     return 0;
