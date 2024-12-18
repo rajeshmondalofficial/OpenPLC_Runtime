1dc9ce4c0759d2f8fda60461f3faa7290342d298
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-18 23:34:25 +0530

    csbdc

--- webserver/core/client.cpp
+++ webserver/core/client.cpp
@@ -21,7 +21,6 @@
 // Thiago Alves, Nov 2022
 //-----------------------------------------------------------------------------
 
-
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
@@ -54,38 +53,40 @@ int global_uart_fd = -1;
 unsigned char inputData[256];
 unsigned char rylr_config_resp[256];
 pthread_mutex_t uart_mutex;  // Mutex for thread-safe access
-unsigned char dataReady = 0;    // Flag for new data
-int uart_listening = -1; // Flag for listening UART
+unsigned char dataReady = 0; // Flag for new data
+int uart_listening = -1;     // Flag for listening UART
 char log_msg[1000];
 
 // RYLR998 Modem Specific Variables
 int mode_connection_id = -1;
 
-
 // UART Initialize
-void uart_init(uint8_t* device) {
-    if(global_uart_fd < 0) {
+void uart_init(uint8_t *device)
+{
+    if (global_uart_fd < 0)
+    {
         // Initialize UART Connection
         global_uart_fd = open(device, O_RDWR | O_NOCTTY);
         sprintf(log_msg, "UART: Connection Initialize: => %d\n", global_uart_fd);
         log(log_msg);
-        if (global_uart_fd < 0) {
+        if (global_uart_fd < 0)
+        {
             perror("Error opening UART device");
             sprintf(log_msg, "UART: Connection Failed: => %d\n", global_uart_fd);
             log(log_msg);
         }
 
-         // Configure UART settings
+        // Configure UART settings
         struct termios options;
         tcgetattr(global_uart_fd, &options);
-    
+
         options.c_cflag = B9600 | CS8 | CLOCAL | CREAD; // Baud rate: 9600, 8 data bits, no parity, 1 stop bit
-        options.c_iflag = IGNPAR;                      // Ignore framing errors
+        options.c_iflag = IGNPAR;                       // Ignore framing errors
         options.c_oflag = 0;
-        options.c_lflag = 0;                           // Non-canonical mode
-        
-        tcflush(global_uart_fd, TCIOFLUSH);                    // Flush the input buffer
-        tcsetattr(global_uart_fd, TCSANOW, &options);         // Apply the configuration
+        options.c_lflag = 0; // Non-canonical mode
+
+        tcflush(global_uart_fd, TCIOFLUSH);           // Flush the input buffer
+        tcsetattr(global_uart_fd, TCSANOW, &options); // Apply the configuration
 
         // Configure UART settings
         // struct termios options;
@@ -93,19 +94,19 @@ void uart_init(uint8_t* device) {
 
         // // Input baud rate
         // cfsetispeed(&options, BAUD_RATE);
-        
+
         // // Output baud rate
         // cfsetospeed(&options, BAUD_RATE);
-        
+
         // // 8 data bits, 1 stop bit, no parity
         // options.c_cflag &= ~PARENB;
         // options.c_cflag &= ~CSTOPB;
         // options.c_cflag &= ~CSIZE;
         // options.c_cflag |= CS8;
-        
+
         // // Enable receiver and ignore modem control lines
         // options.c_cflag |= (CLOCAL | CREAD);
-        
+
         // // Set UART attributes
         // tcsetattr(global_uart_fd, TCSANOW, &options);
         // tcflush(global_uart_fd, TCIOFLUSH);
@@ -117,17 +118,21 @@ void uart_init(uint8_t* device) {
 }
 
 /** UART Communication Block */
-int uart_send(uint8_t* message, uint8_t* device) {
+int uart_send(uint8_t *message, uint8_t *device)
+{
     char termination[] = {0x0D, 0x0A};
-    
-    if(global_uart_fd < 0) {
+
+    if (global_uart_fd < 0)
+    {
         uart_init(device);
         int write_id = write(global_uart_fd, message, strlen(message));
         write(global_uart_fd, termination, strlen(termination));
         // sprintf(log_msg, "UART: Write to connection: => %s\n", inputData);
         // log(log_msg);
         return global_uart_fd;
-    } else {
+    }
+    else
+    {
         int write_id = write(global_uart_fd, message, strlen(message));
         write(global_uart_fd, termination, strlen(termination));
         // sprintf(log_msg, "UART: Write to connection: => %s\n", inputData);
@@ -136,12 +141,15 @@ int uart_send(uint8_t* message, uint8_t* device) {
     }
 }
 
-// Listen to UART 
-void *uart_listener_thread(void *arg) {
+// Listen to UART
+void *uart_listener_thread(void *arg)
+{
     char buffer[256];
-    while (1) {
+    while (1)
+    {
         int bytes_read = read(global_uart_fd, buffer, sizeof(buffer) - 1);
-        if (bytes_read > 0) {
+        if (bytes_read > 0)
+        {
             buffer[bytes_read] = '\0'; // Null-terminate the received string
             // Lock the mutex to update shared data
             pthread_mutex_lock(&uart_mutex);
@@ -149,7 +157,7 @@ void *uart_listener_thread(void *arg) {
             // sprintf(log_msg, "UART: Connection Receive: => %s\n", inputData);
             // log(log_msg);
             inputData[sizeof(inputData) - 1] = '\0'; // Safety null-termination
-            dataReady = 1; // Set flag to indicate data is ready
+            dataReady = 1;                           // Set flag to indicate data is ready
             pthread_mutex_unlock(&uart_mutex);
         }
         // usleep(1000); // Short sleep to avoid busy waiting
@@ -158,30 +166,38 @@ void *uart_listener_thread(void *arg) {
 }
 
 // UART Listen Thread
-void start_uart_thread() {
+void start_uart_thread()
+{
     pthread_t thread_id;
-    if (pthread_create(&thread_id, NULL, uart_listener_thread, NULL) != 0) {
+    if (pthread_create(&thread_id, NULL, uart_listener_thread, NULL) != 0)
+    {
         perror("Failed to create UWslART listener thread");
         uart_listening = -1;
-    } else {
+    }
+    else
+    {
         uart_listening = 0; // Set flag to indicate UART listening
     }
 }
 
 // Process Data Received From UART
-char* uart_listen(uint8_t* device, uint8_t* message, size_t buffer_size) {
-    if(global_uart_fd < 0) {
+char *uart_listen(uint8_t *device, uint8_t *message, size_t buffer_size)
+{
+    if (global_uart_fd < 0)
+    {
         uart_init(device);
     }
-    if(uart_listening < 0) {
+    if (uart_listening < 0)
+    {
         start_uart_thread();
     }
 
     static char message_data[256]; // Static buffer to return data
 
-   // Print buffer contents (example processing)
+    // Print buffer contents (example processing)
     pthread_mutex_lock(&uart_mutex);
-    if (dataReady) {
+    if (dataReady)
+    {
         // sprintf(log_msg, "Received UART Data: => %s\n", inputData);
         // log(log_msg);
         // printf("Received UART Data: %s\n", inputData);
@@ -190,49 +206,55 @@ char* uart_listen(uint8_t* device, uint8_t* message, size_t buffer_size) {
         message_data[sizeof(message_data) - 1] = '\0'; // Ensure null termination
 
         dataReady = 0; // Reset flag after processing
-    } else {
+    }
+    else
+    {
         message_data[0] = '\0'; // Return an empty string if no data is ready
     }
     pthread_mutex_unlock(&uart_mutex);
     return message_data; // Return the received data as a string
 }
 
-int receive_uart_communication(uint8_t* device, uint8_t* message, size_t buffer_size) {
-    if(serial_fd < 0) {
+int receive_uart_communication(uint8_t *device, uint8_t *message, size_t buffer_size)
+{
+    if (serial_fd < 0)
+    {
         serial_fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
-    } 
-    if (serial_fd == -1) { 
-        perror("Unable to open serial port"); 
-        exit(1); 
-    } 
-    struct termios options; 
-    tcgetattr(serial_fd, &options); 
-    
-    // Set baud rate 
-    cfsetispeed(&options, BAUD_RATE); 
-    cfsetospeed(&options, BAUD_RATE); 
-    // Configure 8N1 (8 data bits, no parity, 1 stop bit) 
-    options.c_cflag &= ~PARENB; 
-    options.c_cflag &= ~CSTOPB; 
-    options.c_cflag &= ~CSIZE; 
-    options.c_cflag |= CS8; 
-    // Enable receiver and set local mode 
-    options.c_cflag |= (CLOCAL | CREAD); 
-    // Set timeout and minimum bytes to read 
-    options.c_cc[VMIN] = 0; options.c_cc[VTIME] = 10; 
-    // Timeout in deciseconds 
-    // Apply the configuration 
+    }
+    if (serial_fd == -1)
+    {
+        perror("Unable to open serial port");
+        exit(1);
+    }
+    struct termios options;
+    tcgetattr(serial_fd, &options);
+
+    // Set baud rate
+    cfsetispeed(&options, BAUD_RATE);
+    cfsetospeed(&options, BAUD_RATE);
+    // Configure 8N1 (8 data bits, no parity, 1 stop bit)
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    // Enable receiver and set local mode
+    options.c_cflag |= (CLOCAL | CREAD);
+    // Set timeout and minimum bytes to read
+    options.c_cc[VMIN] = 0;
+    options.c_cc[VTIME] = 10;
+    // Timeout in deciseconds
+    // Apply the configuration
     tcsetattr(serial_fd, TCSANOW, &options);
 
     char log_msg[1000];
 
-    int bytes_read = read(serial_fd, message, buffer_size);  
+    int bytes_read = read(serial_fd, message, buffer_size);
     message[bytes_read] = 0;
 
     // if(listening == 0) {
     //     uart_listen(message, buffer_size);
     // }
-    
+
     return bytes_received;
 }
 
@@ -241,12 +263,12 @@ int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method)
     int sockfd, connfd;
     char log_msg[1000];
     struct sockaddr_in servaddr, cli;
-    
+
     if (method == METHOD_TCP)
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
     else if (method == METHOD_UDP)
         sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    
+
     if (sockfd == -1)
     {
         sprintf(log_msg, "TCP Client: error creating TCP socket => %s\n", strerror(errno));
@@ -254,13 +276,13 @@ int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method)
         return -1;
     }
     bzero(&servaddr, sizeof(servaddr));
-    
-    //Configure socket
+
+    // Configure socket
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(ip_address);
     servaddr.sin_port = htons(port);
-    
-    //Connect to server
+
+    // Connect to server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
     {
         sprintf(log_msg, "TCP Client: error connecting to server => %s\n", strerror(errno));
@@ -268,8 +290,8 @@ int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method)
         close(sockfd);
         return -1;
     }
-    
-    //Set non-blocking socket
+
+    // Set non-blocking socket
     int flags = fcntl(sockfd, F_GETFL, 0);
     if (flags == -1)
     {
@@ -284,7 +306,7 @@ int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method)
         log(log_msg);
         return -1;
     }
-    
+
     return sockfd;
 }
 
@@ -299,7 +321,7 @@ int send_tcp_message(uint8_t *msg, size_t msg_size, int socket_id)
         log(log_msg);
         return -1;
     }
-    
+
     return bytes_sent;
 }
 
@@ -308,11 +330,11 @@ int receive_tcp_message(uint8_t *msg_buffer, size_t buffer_size, int socket_id)
     int bytes_received = 0;
     char log_msg[1000];
     bytes_received = read(socket_id, msg_buffer, buffer_size);
-    
+
     if (bytes_received < 0 && bytes_received != EAGAIN && bytes_received != EWOULDBLOCK)
     {
-        //sprintf(log_msg, "TCP Client: error receiving msg from server => %s\n", strerror(errno));
-        //log(log_msg);
+        // sprintf(log_msg, "TCP Client: error receiving msg from server => %s\n", strerror(errno));
+        // log(log_msg);
         return -1;
     }
     else
@@ -321,7 +343,7 @@ int receive_tcp_message(uint8_t *msg_buffer, size_t buffer_size, int socket_id)
         sprintf(log_msg, "TCP Client: msg from server => %s\n", msg_buffer);
         log(log_msg);
     }
-    
+
     return bytes_received;
 }
 
@@ -330,24 +352,24 @@ int close_tcp_connection(int socket_id)
     return close(socket_id);
 }
 
-
-
 // Codes for Lyra998 Modem Specific Code Blocks
-// Get UART Connection for 
-int get_uart_connection(uint8_t *device, int baud_rate) {
-    if(mode_connection_id < 0) {
+// Get UART Connection for
+int get_uart_connection(uint8_t *device, int baud_rate)
+{
+    if (mode_connection_id < 0)
+    {
         mode_connection_id = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
         // Configure UART settings
         struct termios options;
         tcgetattr(global_uart_fd, &options);
-    
+
         options.c_cflag = B9600 | CS8 | CLOCAL | CREAD; // Baud rate: 9600, 8 data bits, no parity, 1 stop bit
-        options.c_iflag = IGNPAR;                      // Ignore framing errors
+        options.c_iflag = IGNPAR;                       // Ignore framing errors
         options.c_oflag = 0;
-        options.c_lflag = 0;                           // Non-canonical mode
-        
-        tcflush(global_uart_fd, TCIOFLUSH);                    // Flush the input buffer
-        tcsetattr(global_uart_fd, TCSANOW, &options); 
+        options.c_lflag = 0; // Non-canonical mode
+
+        tcflush(global_uart_fd, TCIOFLUSH); // Flush the input buffer
+        tcsetattr(global_uart_fd, TCSANOW, &options);
 
         pthread_mutex_init(&uart_mutex, NULL);
 
@@ -357,16 +379,18 @@ int get_uart_connection(uint8_t *device, int baud_rate) {
 }
 
 // RYLR Configuration Block
-int rylr998_config(uint8_t *device, int baud_rate, int frequency) {
+int rylr998_config(uint8_t *device, int baud_rate, int frequency)
+{
     int connection_id = get_uart_connection(device, baud_rate);
-    if(connection_id < 0) {
+    if (connection_id < 0)
+    {
         return 0;
     }
     char termination[] = {0x0D, 0x0A};
     // char at_command[256] = "AT+ADDRESS=19\r\n";
     char at_command[256];
     char msg_buffer[256];
-    char numStr[32]; 
+    char numStr[32];
     char log_msg[1024];
 
     // Convert the integer to a string
@@ -381,22 +405,24 @@ int rylr998_config(uint8_t *device, int baud_rate, int frequency) {
 
     int byte_read = read(connection_id, msg_buffer, sizeof(msg_buffer) - 1);
     strncpy(rylr_config_resp, msg_buffer, sizeof(rylr_config_resp) - 1);
-    
-    
-    if(byte_read > 0) {
+
+    if (byte_read > 0)
+    {
         msg_buffer[byte_read] = '\0';
         sprintf(log_msg, "RYLR: Received Bytes => %s\n", rylr_config_resp);
         log(log_msg);
-        tcflush(connection_id, TCIOFLUSH); 
+        tcflush(connection_id, TCIOFLUSH);
         return connection_id;
     }
     return connection_id;
 }
 
 // RYLR Received Block
-void rylr_receive() {
+void rylr_receive()
+{
     int connection_id = get_uart_connection(UART_DEVICE, 9600);
-    if(connection_id < 0) {
+    if (connection_id < 0)
+    {
         log("Couldn't get connection");
         return;
     }
@@ -404,49 +430,47 @@ void rylr_receive() {
     char buffer[BUFFER_SIZE];
     int index = 0;
 
-    while (1) {
-        fd_set read_fds;
-        struct timeval timeout;
+    if (uart_listening < 0)
+    {
+        while (1)
+        {
+            fd_set read_fds;
+            struct timeval timeout;
 
-        FD_ZERO(&read_fds);
-        FD_SET(connection_id, &read_fds);
+            FD_ZERO(&read_fds);
+            FD_SET(connection_id, &read_fds);
 
-        timeout.tv_sec = 15;  // Timeout of 2 seconds
-        timeout.tv_usec = 0;
+            timeout.tv_sec = 15; // Timeout of 2 seconds
+            timeout.tv_usec = 0;
 
-        if (select(connection_id + 1, &read_fds, NULL, NULL, &timeout) > 0) {
-            if (FD_ISSET(connection_id, &read_fds)) {
-                char temp;
-                if (read(connection_id, &temp, 1) > 0) {
-                    if (temp == '\n') { // End of packet
-                        buffer[index] = '\0';
-                        sprintf(log_msg, "RYLR: Received Bytes => %s\n", buffer);
-                        log(log_msg);
-                        // printf("Received packet: %s\n", buffer);
-                        index = 0;
-                    } else if (index < BUFFER_SIZE - 1) {
-                        buffer[index++] = temp;
+            if (select(connection_id + 1, &read_fds, NULL, NULL, &timeout) > 0)
+            {
+                if (FD_ISSET(connection_id, &read_fds))
+                {
+                    char temp;
+                    if (read(connection_id, &temp, 1) > 0)
+                    {
+                        if (temp == '\n')
+                        { // End of packet
+                            buffer[index] = '\0';
+                            sprintf(log_msg, "RYLR: Received Bytes => %s\n", buffer);
+                            log(log_msg);
+                            // printf("Received packet: %s\n", buffer);
+                            index = 0;
+                        }
+                        else if (index < BUFFER_SIZE - 1)
+                        {
+                            buffer[index++] = temp;
+                        }
                     }
                 }
             }
-        } else {
-            log("No data received within timeout\n");
-            printf("No data received within timeout\n");
+            else
+            {
+                log("No data received within timeout\n");
+                printf("No data received within timeout\n");
+            }
+            uart_listening = 1
         }
     }
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
