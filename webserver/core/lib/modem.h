e5daee44ecacff90c13bf2a1e5b3a18c805f1d36
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-28 19:24:59 +0530

    thy

--- webserver/core/lib/modem.h
+++ webserver/core/lib/modem.h
@@ -171,9 +171,9 @@ static void RYLR998_RECEIVE_body__(RYLR998_RECEIVE *data__)
       // char *message_payload = strtok_r(rest_after_length, ",", &saveptr4);
       // char *rest_after_message = strtok_r(NULL, ",", &saveptr4);
       // Set Message
-      strncpy((char *)message.body, rest_after_length, strlen(rest_after_length)); // Copy data to body
-      message.body[strlen(rest_after_length)] = '\0';                            // Null-terminate
-      message.len = (uint8_t)strlen(rest_after_length);
+      strncpy((char *)message.body, receive_message, strlen(receive_message)); // Copy data to body
+      message.body[strlen(receive_message)] = '\0';                            // Null-terminate
+      message.len = (uint8_t)strlen(receive_message);
 
       // Set Address
       strncpy((char *)address.body, address_payload, strlen(address_payload)); // Copy data to body
